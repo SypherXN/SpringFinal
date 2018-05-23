329acfbd4b54b7143f66c7d79715c0e1c6df24d5
Matthew Tran <mattgtran2001@gmail.com>
2018-05-23 13:29:11 -0700

    FIXED RANDOM WIN CHANCE

--- src/Main/MainGame.java
+++ src/Main/MainGame.java
@@ -17,37 +17,34 @@ public class MainGame {
 		System.out.println("Would you like to try to win the game now?");	
 		String response = input.nextLine();
 		
-		if(response.toLowerCase().equals("yes")) {
+		if (response.toLowerCase().equals("yes")) {
 			
-			int win = (int)Math.random() * 100;
+			int win = (int)(Math.random() * 100);
 			
-			if(win == 0) {
+			if (win == 0) {
 				
 				System.out.println("You won the game. It must have been easy.");
-				
-			}
-			
-		} else {
-			
-			System.out.println("Too bad. You could have won!");
-			
-			PlayerInfo.initPlayer();
-			Location[] arr = {new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation()};
-			Map.initMap(arr);
-			
-			Information.initResistances();
-			Information.initWeaknesses();
-			Information.initEnemiesLocation(arr);
-			
-			while(PlayerInfo.getHP() > 0) {
-				
-				Menu.mainMenu();
+				System.exit(0);
 				
 			}
 			
 		}
+			
+		System.out.println("Too bad. You could have won!");
 		
+		PlayerInfo.initPlayer();
+		Location[] arr = {new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation(), new TestLocation()};
+		Map.initMap(arr);
 		
+		Information.initResistances();
+		Information.initWeaknesses();
+		Information.initEnemiesLocation(arr);
+		
+		while(PlayerInfo.getHP() > 0) {
+			
+			Menu.mainMenu();
+			
+		}
 		
 	}
 	
